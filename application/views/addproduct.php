c69c81e4975728d678926bebf114cd017c194f80
Hasika Dilshani <hasikadilshani@gmail.com>
2019-06-21 07:31:21 +0530

    Update addproduct.php

--- application/views/addproduct.php
+++ application/views/addproduct.php
@@ -11,6 +11,11 @@
     crossorigin="anonymous">
     </script>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+     <!-- Compiled and minified JavaScript -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="utf-8">
     <title>Add a New Product </title>
