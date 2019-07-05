f08590b2ff66f6d06afae8d5040cd0b9dce4e15c
Hasika Dilshani <hasikadilshani@gmail.com>
2019-07-05 11:42:38 +0530

    Create gallery.php

---
+++ application/views/gallery.php
@@ -0,0 +1,8 @@
+
+  <div class="carousel">
+    <a class="carousel-item" href="#one!"><img src="https://5.imimg.com/data5/CL/FA/MY-33930774/marine-aquarium-500x500.jpg"></a>
+    <a class="carousel-item" href="#two!"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN86CMEcZ1ayT_Svd4cIPnsqPJEux38f3AumGUPSa18b--j_Aw"></a>
+    <a class="carousel-item" href="#three!"><img src="https://i.pinimg.com/originals/44/a1/ca/44a1ca910928dd5cd9943059fa3c75d6.jpg"></a>
+    <a class="carousel-item" href="#four!"><img src="https://my-test-11.slatic.net/p/206de2fa30ba9e5c920524ef2098b9a2.jpg_340x340q80.jpg_.webp"></a>
+    <a class="carousel-item" href="#five!"><img src="http://www.fschicos.com/upLoad/product/month_1803/201803301131518050.jpg"></a>
+  </div>
